38617ae786929a5627824e60c87c69046319da7a
Xiang Zhang <xiang@daimon.chat>
2022-05-06 20:08:37 -0400

    Implemented ATNIAE model

--- xjax/model.py
+++ xjax/model.py
@@ -8,15 +8,15 @@ from xjax.xmod import vjp, vjp_full, vjp_inputs, map_ones_like, map_add
 
 
 def ATNNFAE(enc, dec, disc, inj, rnd, ae_loss, gen_loss, disc_loss):
-    """Adversarially-Trained Normalized Noisy-Feature Auto-Encoder model.
+    """Adversarially-Trained Normalized Noisy-Feature Auto-Encoder.
 
     Args:
       enc: the encoder module.
       dec: the decoder / generator module.
       disc: the discriminator module.
-      inj: the noise injection modle.
+      inj: the noise injection module.
       rnd: the random noise generation module.
-      ae_loss: the autoencode loss.
+      ae_loss: the autoencoder loss.
       gen_loss: the generator loss.
       disc_loss: the discriminator loss.
 
@@ -24,8 +24,8 @@ def ATNNFAE(enc, dec, disc, inj, rnd, ae_loss, gen_loss, disc_loss):
       forward: the forward function that returns net_outputs, loss_outputs, and
         states.
       backward: the backward function that returns grads and forward returns.
-      initial_params: the initial parameters from net.
-      initial_states: the initial states.
+      params: the initial parameters.
+      states: the initial states.
     """
     initial_params = (enc[1], dec[1], disc[1])
     initial_states = (enc[2], dec[2], disc[2], inj[2], rnd[2], ae_loss[2],
@@ -65,9 +65,9 @@ def ATNNFAE(enc, dec, disc, inj, rnd, ae_loss, gen_loss, disc_loss):
             gen_loss_params, fake_outputs, gen_loss_states)
         disc_loss_outputs, disc_loss_states = disc_loss_forward(
             disc_loss_params, [real_outputs, fake_outputs], disc_loss_states)
+        loss_outputs = [ae_loss_outputs, gen_loss_outputs, disc_loss_outputs]
         states = (enc_states, dec_states, disc_states, inj_states, rnd_states,
                   ae_loss_states, gen_loss_states, disc_loss_states)
-        loss_outputs = [ae_loss_outputs, gen_loss_outputs, disc_loss_outputs]
         return net_outputs, loss_outputs, states
     def backward(params, inputs, states):
         enc_params, dec_params, disc_params = params
@@ -127,5 +127,115 @@ def ATNNFAE(enc, dec, disc, inj, rnd, ae_loss, gen_loss, disc_loss):
         grads_disc_params = map_add(
             grads_disc_params_real, grads_disc_params_fake)
         grads = (grads_enc_params, grads_dec_params, grads_disc_params)
+        return grads, net_outputs, loss_outputs, states
+    return ModelTuple(forward, backward, initial_params, initial_states)
+
+
+def ATNIAE(net, disc, inj, rnd, ae_loss, gen_loss, disc_loss):
+    """Adversarially-Trained Noisy-Input Auto-Encoder.
+
+    Args:
+      net: the autoencoder module.
+      disc: the discriminator module.
+      inj: the noise injection module.
+      rnd: the random noise generation module.
+      ae_loss: the autoencoder loss.
+      gen_loss: the generator loss.
+      disc_loss: the discriminator loss.
+
+    Returns:
+      forward: the forward function that returns net_outputs, loss_outputs, and
+        states.
+      backward: the backward function that returns grads and forward returns.
+      params: the initial parameters.
+      states: the initial states.
+    """
+    initial_params = (net[1], disc[1])
+    initial_states = (net[2], disc[2], inj[2], rnd[2], ae_loss[2], gen_loss[2],
+                      disc_loss[2])
+    net_forward, disc_forward = net[0], disc[0]
+    inj_forward, inj_params, _ = inj
+    rnd_forward, rnd_params, _ = rnd
+    ae_loss_forward, ae_loss_params, _ = ae_loss
+    gen_loss_forward, gen_loss_params, _ = gen_loss
+    disc_loss_forward, disc_loss_params, _ = disc_loss
+    def forward(params, inputs, states):
+        net_params, disc_params = params
+        net_states, disc_states, inj_states, rnd_states = states[:4]
+        ae_loss_states, gen_loss_states, disc_loss_states = states[4:]
+        targets, weights = inputs
+        tar_outputs, net_states = net_forward(net_params, targets, net_states)
+        inj_outputs, inj_states = inj_forward(inj_params, targets, inj_states)
+        dec_outputs, net_states = net_forward(
+            net_params, inj_outputs, net_states)
+        rnd_outputs, rnd_states = rnd_forward(rnd_params, targets, rnd_states)
+        gen_outputs, net_states = net_forward(
+            net_params, rnd_outputs, net_states)
+        real_outputs, disc_states = disc_forward(
+            disc_params, tar_outputs, disc_states)
+        fake_outputs, disc_states = disc_forward(
+            disc_params, gen_outputs, disc_states)
+        net_outputs = [tar_outputs, dec_outputs, gen_outputs,
+                       real_outputs, fake_outputs]
+        ae_loss_outputs, ae_loss_states = ae_loss_forward(
+            ae_loss_params, [dec_outputs, targets, weights], ae_loss_states)
+        gen_loss_outputs, gen_loss_states = gen_loss_forward(
+            gen_loss_params, fake_outputs, gen_loss_states)
+        disc_loss_outputs, disc_loss_states = disc_loss_forward(
+            disc_loss_params, [real_outputs, fake_outputs], disc_loss_states)
+        loss_outputs = [ae_loss_outputs, gen_loss_outputs, disc_loss_outputs]
+        states = (net_states, disc_states, inj_states, rnd_states,
+                  ae_loss_states, gen_loss_states, disc_loss_states)
+        return net_outputs, loss_outputs, states
+    def backward(params, inputs, states):
+        net_params, disc_params = params
+        net_states, disc_states, inj_states, rnd_states = states[:4]
+        ae_loss_states, gen_loss_states, disc_loss_states = states[4:]
+        targets, weights = inputs
+        # Forward propagate and build backward graph.
+        tar_outputs, net_states = net_forward(net_params, targets, net_states)
+        inj_outputs, inj_states = inj_forward(inj_params, targets, inj_states)
+        net_vjpf, dec_outputs, net_states = vjp(
+            net_forward, net_params, inj_outputs, net_states)
+        rnd_outputs, rnd_states = rnd_forward(rnd_params, targets, rnd_states)
+        gen_vjpf, gen_outputs, gen_states = vjp(
+            net_forward, net_params, rnd_outputs, net_states)
+        disc_vjpf_real, real_outputs, disc_states = vjp(
+            disc_forward, disc_params, tar_outputs, disc_states)
+        disc_vjpf_fake, fake_outputs, disc_states = vjp_full(
+            disc_forward, disc_params, gen_outputs, disc_states)
+        net_outputs = [tar_outputs, dec_outputs, gen_outputs,
+                       real_outputs, fake_outputs]
+        ae_loss_vjpf, ae_loss_outputs, ae_loss_states = vjp_inputs(
+            ae_loss_forward, ae_loss_params, [dec_outputs, targets, weights],
+            ae_loss_states)
+        gen_loss_vjpf, gen_loss_outputs, gen_loss_states = vjp_inputs(
+            gen_loss_forward, gen_loss_params, fake_outputs, gen_loss_states)
+        disc_loss_vjpf, disc_loss_outputs, disc_loss_states = vjp_inputs(
+            disc_loss_forward, disc_loss_params, [real_outputs, fake_outputs],
+            disc_loss_states)
+        loss_outputs = [ae_loss_outputs, gen_loss_outputs, disc_loss_outputs]
+        states = (net_states, disc_states, inj_states, rnd_states,
+                  ae_loss_states, gen_loss_states, disc_loss_states)
+        # Backward propagate to autoencoder.
+        grads_ae_loss_outputs = map_ones_like(ae_loss_outputs)
+        grads_dec_outputs, _, _ = ae_loss_vjpf(grads_ae_loss_outputs)
+        grads_net_params = net_vjpf(grads_dec_outputs)
+        # Backward propagate to generator.
+        grads_gen_loss_outputs = map_ones_like(gen_loss_outputs)
+        grads_fake_outputs_gen = gen_loss_vjpf(grads_gen_loss_outputs)
+        _, grads_gen_outputs = disc_vjpf_fake(grads_fake_outputs_gen)
+        grads_gen_params = gen_vjpf(grads_gen_outputs)
+        # Backward propagate to discriminator
+        grads_disc_loss_outputs = map_ones_like(disc_loss_outputs)
+        grads_real_outputs, grads_fake_outputs_disc = (
+            disc_loss_vjpf(grads_disc_loss_outputs))
+        grads_disc_params_real = disc_vjpf_real(grads_real_outputs)
+        grads_disc_params_fake, _ = disc_vjpf_fake(grads_fake_outputs_disc)
+        # Add parameters together
+        grads_net_params = map_add(grads_net_params, grads_gen_params)
+        grads_disc_params = map_add(
+            grads_disc_params_real, grads_disc_params_fake)
+        grads = (grads_net_params, grads_disc_params)
         return grads, net_outputs, loss_outputs, states
     return ModelTuple(forward, backward, initial_params, initial_states)
--- xjax/model_test.py
+++ xjax/model_test.py
@@ -1,6 +1,6 @@
 """Unit tests for model module."""
 
-from model import ATNNFAE
+from model import ATNNFAE, ATNIAE
 
 import math
 
@@ -167,6 +167,175 @@ class ATNNFAETest(absltest.TestCase):
         ref_grads = ref_backward_disc(params, inputs, states)
         jax.tree_map(lambda x, y: self.assertTrue(jnp.allclose(x, y)),
                      ref_grads[2], grads[2])
+
+    def test_vmap(self):
+        forward, backward, params, states = xmod.vmap(self.model, 2)
+        inputs = [jrand.normal(xrand.split(), (2, 8)),
+                  jrand.normal(xrand.split(), (2,))]
+        net_outputs, loss_outputs, states = forward(params, inputs, states)
+        [tar_outputs, dec_outputs, gen_outputs, real_outputs,
+         fake_outputs] = net_outputs
+        ae_loss_outputs, gen_loss_outputs, disc_loss_outputs = loss_outputs
+        self.assertEqual((2, 8), tar_outputs.shape)
+        self.assertEqual((2, 8), dec_outputs.shape)
+        self.assertEqual((2, 8), gen_outputs.shape)
+        self.assertEqual((2,), real_outputs.shape)
+        self.assertEqual((2,), fake_outputs.shape)
+        self.assertEqual((2,), ae_loss_outputs.shape)
+        self.assertEqual((2,), gen_loss_outputs.shape)
+        self.assertEqual((2,), disc_loss_outputs.shape)
+        grads, net_outputs, loss_outputs, states = backward(
+            params, inputs, states)
+        jax.tree_map(lambda p, g: self.assertEqual((2,) + p.shape, g.shape),
+                     params, grads)
+
+
+class ATNIAETest(absltest.TestCase):
+    def setUp(self):
+        # Encoder is a 2-layer MLP.
+        enc = xnn.Sequential(
+            xnn.Linear(8, 16), xnn.ReLU(), xnn.Linear(16, 4), xnn.Standardize())
+        # Decoder / generator is a 2-layer MLP.
+        dec = xnn.Sequential(
+            xnn.Standardize(), xnn.Linear(4, 16), xnn.ReLU(), xnn.Linear(16, 8))
+        self.net = xnn.Sequential(enc, dec)
+        # Discriminator is a 2-layer MLP.
+        self.disc = xnn.Sequential(
+            xnn.Linear(8, 16), xnn.ReLU(), xnn.Linear(16, 1), xnn.Mean())
+        # Noise injector adds noise from normal distribution.
+        self.inj = xnn.Sequential(
+            # inputs -> [inputs] -> [inputs, [inputs, inputs]]
+            xnn.Pack(), xnn.Group([0, [0, 0]]),
+            # [inputs, [inputs, inputs]] -> [inputs, noise]
+            xnn.Parallel(
+                # inputs -> inputs
+                xnn.Identity(),
+                # [inputs, inputs] -> noise
+                xnn.Sequential(
+                    # [inputs, inputs] -> [noise, scale]
+                    xnn.Parallel(
+                        xnn.NormalLike(),
+                        xnn.Sequential(xnn.Exponential(), xnn.MulConst(0.1))),
+                    # [noise, scale] -> noise
+                    xnn.Multiply())),
+            # [inputs, noise] -> [inputs + noise]
+            xnn.Add())
+        # Random generator.
+        self.rnd = xnn.NormalLike()
+        # Auto-encoder loss is square loss.
+        self.ae_loss = xnn.Sequential(
+            # [outputs, targets, weights] -> [[outputs, targets], weights]
+            xnn.Group([[0, 1], 2]),
+            # [[outputs, targets], weights] -> [loss, weights]
+            xnn.Parallel(
+                # [outputs, targets] -> loss
+                xnn.Sequential(xnn.Subtract(), xnn.Square(), xnn.Sum()),
+                # weights -> weights
+                xnn.Identity()),
+            # [loss, weights] -> weight
+            xnn.Multiply())
+        # Generator loss is log-cosh loss towards 0.
+        self.gen_loss = xnn.Sequential(
+            # inputs -> [inputs] -> [inputs, inputs]
+            xnn.Pack(), xnn.Group([0, 0]),
+            # [inputs, inputs] -> [inputs, zeros]
+            xnn.Parallel(xnn.Identity(), xnn.ZerosLike()),
+            # [inputs, zeros] -> logcosh
+            xnn.LogCosh(), xnn.Mean())
+        # Discriminator is log-cosh loss towards 0 for real, and -1 for fake.
+        self.disc_loss = xnn.Sequential(
+            # [real, fake] -> [[real, real], [fake, fake]]
+            xnn.Group([[0, 0], [1, 1]]),
+            # [[real, real], [fake, fake]] -> [real_logcosh, fake_logcosh]
+            xnn.Parallel(
+                # [real, real] -> real_logcosh
+                xnn.Sequential(
+                    # [real, real] -> [real, zeros]
+                    xnn.Parallel(xnn.Identity(), xnn.ZerosLike()),
+                    # [real, zeros] -> real_logcosh
+                    xnn.LogCosh()),
+                # [fake, fake] -> fake_logcosh
+                xnn.Sequential(
+                    # [fake, fake] -> [fake, -ones]
+                    xnn.Parallel(xnn.Identity(), xnn.FullLike(-1)),
+                    # [fake, -ones] -> fake_logcosh
+                    xnn.LogCosh())),
+            # [real_logcosh, fake_logcosh] -> loss
+            xnn.Add(), xnn.Mean())
+        # Build the model
+        self.model = ATNIAE(self.net, self.disc, self.inj, self.rnd,
+                            self.ae_loss, self.gen_loss, self.disc_loss)
+        # Inputs is a vector of size 8.
+        self.inputs = [jrand.normal(xrand.split(), shape=(8,)), 1.1]
+
+
+    def test_forward(self):
+        forward, _, params, states = self.model
+        inputs = self.inputs
+        net_outputs, loss_outputs, states = forward(params, inputs, states)
+        (tar_outputs, dec_outputs, gen_outputs, real_outputs,
+         fake_outputs) = net_outputs
+        ae_loss_outputs, gen_loss_outputs, disc_loss_outputs = loss_outputs
+        net_forward, net_params, net_states = self.net
+        ref_tar_outputs, dec_states = net_forward(
+            net_params, inputs[0], net_states)
+        self.assertTrue(jnp.allclose(ref_tar_outputs, tar_outputs))
+        inj_forward, inj_params, inj_states = self.inj
+        inj_outputs, inj_states = inj_forward(
+            inj_params, inputs[0], inj_states)
+        ref_dec_outputs, net_states = net_forward(
+            net_params, inj_outputs, net_states)
+        self.assertTrue(jnp.allclose(ref_dec_outputs, dec_outputs))
+        rnd_forward, rnd_params, rnd_states = self.rnd
+        rnd_outputs, rng_states = rnd_forward(
+            rnd_params, inputs[0], rnd_states)
+        ref_gen_outputs, net_states = net_forward(
+            net_params, rnd_outputs, net_states)
+        self.assertTrue(jnp.allclose(ref_gen_outputs, gen_outputs))
+        disc_forward, disc_params, disc_states = self.disc
+        ref_real_outputs, disc_states = disc_forward(
+            disc_params, tar_outputs, disc_states)
+        self.assertTrue(jnp.allclose(ref_real_outputs, real_outputs))
+        ref_fake_outputs, disc_states = disc_forward(
+            disc_params, gen_outputs, disc_states)
+        self.assertTrue(jnp.allclose(ref_fake_outputs, fake_outputs))
+        ae_loss_forward, ae_loss_params, ae_loss_states = self.ae_loss
+        ref_ae_loss_outputs, ae_loss_states = ae_loss_forward(
+            ae_loss_params, [dec_outputs, inputs[0], inputs[1]], ae_loss_states)
+        self.assertTrue(jnp.allclose(ref_ae_loss_outputs, ae_loss_outputs))
+        gen_loss_forward, gen_loss_params, gen_loss_states = self.gen_loss
+        ref_gen_loss_outputs, gen_loss_states = gen_loss_forward(
+            gen_loss_params, fake_outputs, ae_loss_states)
+        self.assertTrue(jnp.allclose(ref_gen_loss_outputs, gen_loss_outputs))
+        disc_loss_forward, disc_loss_params, disc_loss_states = self.disc_loss
+        ref_disc_loss_outputs, disc_loss_states = disc_loss_forward(
+            disc_loss_params, [real_outputs, fake_outputs], disc_loss_states)
+        self.assertTrue(jnp.allclose(ref_disc_loss_outputs, disc_loss_outputs))
+
+    def test_backward(self):
+        forward, backward, params, states = self.model
+        inputs = self.inputs
+        grads, net_outputs, loss_outputs, _ = backward(
+            params, inputs, states)
+        ref_net_outputs, ref_loss_outputs, _ = forward(
+            params, inputs, states)
+        jax.tree_map(lambda x, y: self.assertTrue(jnp.allclose(x, y)),
+                     [ref_net_outputs, ref_loss_outputs],
+                     [net_outputs, loss_outputs])
+        def ref_forward_net(params, inputs, states):
+            _, loss_outputs, _ = forward(params, inputs, states)
+            return loss_outputs[0] + loss_outputs[1]
+        ref_backward_net = jax.grad(ref_forward_net)
+        ref_grads = ref_backward_net(params, inputs, states)
+        jax.tree_map(lambda x, y: self.assertTrue(jnp.allclose(x, y)),
+                     ref_grads[0], grads[0])
+        def ref_forward_disc(params, inputs, states):
+            _, loss_outputs, _ = forward(params, inputs, states)
+            return loss_outputs[2]
+        ref_backward_disc = jax.grad(ref_forward_disc)
+        ref_grads = ref_backward_disc(params, inputs, states)
+        jax.tree_map(lambda x, y: self.assertTrue(jnp.allclose(x, y)),
+                     ref_grads[1], grads[1])
 
     def test_vmap(self):
         forward, backward, params, states = xmod.vmap(self.model, 2)
